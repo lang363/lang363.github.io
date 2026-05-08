d4d0fea8f11105acc9a5a7468c1715c9d40304f6
lang363 <2136075589@qq.com>
2026-05-08 13:16:13 +0800

    Update Theme.js

--- static/plugins/Theme.js
+++ static/plugins/Theme.js
@@ -496,14 +496,35 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         
         /* 代码块 */
-        .markdown-body .highlight pre, .markdown-body pre {
+        /*.markdown-body .highlight pre, .markdown-body pre {
             color: rgb(0, 0, 0);          /* 代码块内代码颜色 */
             background-color: rgba(245, 246, 248, 0.92);
             border: 1px solid rgba(255, 255, 255, 0.45);
             box-shadow: 0 16px 40px rgba(0, 0, 0, 0.12);
             padding-top: 20px; 
             border-radius: 8px;
-        }
+        }*/
+        [data-color-mode=light][data-light-theme=dark],
+[data-color-mode=light][data-light-theme=dark]::selection,
+[data-color-mode=dark][data-dark-theme=dark],
+[data-color-mode=dark][data-dark-theme=dark]::selection {
+    --markdown-pre-bgColor: #27282d;/* 增加 */
+	--markdown-pre-shadowColor: #00000026;/* 增加 */
+}
+:root {
+    --markdown-pre-bgColor: #f6f8fa;/* 增加 */
+	--markdown-pre-shadowColor: #5f5f5f26;/* 增加 */
+}
+.markdown-body .highlight pre, .markdown-body pre {
+	padding: 16px;
+	overflow: auto;
+	font-size: 85%;
+	line-height: 1.45;
+	color: var(--fgColor-default, var(--color-fg-default));
+	border-radius: 6px;
+	background-color: var(--markdown-pre-bgColor);/* 增加 */
+	box-shadow: 4px 5px 14px 0 var(--markdown-pre-shadowColor) inset;/* 增加 */
+}
 
         /* 行内代码 */
         .markdown-body code, .markdown-body tt {
